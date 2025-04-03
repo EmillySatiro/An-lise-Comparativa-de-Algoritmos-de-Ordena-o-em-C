c8bb6642ecadade1002c1630b935d945e95bbada
WalisonWeudes <weudesw0@gmail.com>
2025-04-02 23:49:59 -0300

    feat: adicionar configurações de compilação e ajustes no teste de algoritmos de ordenação

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #include "cocktail_shaker_sort/cocktail_shaker_sort.h"
 #include "utils/utils.h"
 
+#define REPETICOES 30
+
 // Função para testar um algoritmo de ordenação e retornar resultados
 void testar_algoritmo(const char* nome_algoritmo, int sizes[], int num_sizes, 
                      void (*algoritmo)(int*, int)) {
@@ -14,70 +16,99 @@ void testar_algoritmo(const char* nome_algoritmo, int sizes[], int num_sizes,
     double temp_cresc[num_sizes];
     double temp_dec[num_sizes];
     double temp_rand[num_sizes];
+
+    double soma_cresc[num_sizes];
+    double soma_dec[num_sizes];
+    double soma_rand[num_sizes];
+
+     // Inicializando as somas
+     for (int i = 0; i < num_sizes; i++) {
+        soma_cresc[i] = 0.0;
+        soma_dec[i] = 0.0;
+        soma_rand[i] = 0.0;
+    }
+
     
     printf("===================== TESTE DE ORDENAÇÃO =====================\n");
     printf("Algoritmo: %s\n", nome_algoritmo);
     printf("-------------------------------------------------------------\n");
     printf("| Tamanho | Crescente (ms) | Decrescente (ms) | Aleatório (ms) |\n");
     printf("-------------------------------------------------------------\n");
-    
-    for (int i = 0; i < num_sizes; i++) {
-        int size = sizes[i];
-        
-        // Criando os vetores de teste
-        int* crescente = criar_array_cresecente(size);
-        int* decrescente = criar_array_decrescente(size);
-        int* random = criar_array_aleatorio(size);
-        
-        // Criando cópias para ordenação
-        int* cres_cpy = copiar_array(crescente, size);
-        int* dec_cpy = copiar_array(decrescente, size);
-        int* rand_cpy = copiar_array(random, size);
+    for (int exec = 0; exec < REPETICOES; exec++) {
+   
+        for (int i = 0; i < num_sizes; i++) {
+            int size = sizes[i];
+            
+            // Criando os vetores de teste
+            int* crescente = criar_array_cresecente(size);
+            int* decrescente = criar_array_decrescente(size);
+            int* random = criar_array_aleatorio(size);
+            
+            // Criando cópias para ordenação
+            int* cres_cpy = copiar_array(crescente, size);
+            int* dec_cpy = copiar_array(decrescente, size);
+            int* rand_cpy = copiar_array(random, size);
+            
+            // Medindo tempo para cada caso
+            temp_cresc[i] = calcular_tempo(cres_cpy, size, algoritmo);
+            temp_dec[i] = calcular_tempo(dec_cpy, size, algoritmo);
+            temp_rand[i] = calcular_tempo(rand_cpy, size, algoritmo);
+            
+            // Verificando se os vetores foram ordenados corretamente
+            int cresc_sorted = esta_ordenado(cres_cpy, size);
+            int dec_sorted = esta_ordenado(dec_cpy, size);
+            int rand_sorted = esta_ordenado(rand_cpy, size);
+            
+            // Imprimindo resultados
+            printf("| %7d | %14.2f | %15.2f | %13.2f |\n", 
+                size, temp_cresc[i], temp_dec[i], temp_rand[i]);
+                
+                // Verificação de ordenação
+                if (!cresc_sorted || !dec_sorted || !rand_sorted) {
+                    printf("ERRO: Falha na ordenação para o tamanho %d\n", size);
+                    if (!cresc_sorted) printf("  - Array crescente não ordenado corretamente\n");
+                    if (!dec_sorted) printf("  - Array decrescente não ordenado corretamente\n");
+                    if (!rand_sorted) printf("  - Array aleatório não ordenado corretamente\n");
+                }
 
-        // Medindo tempo para cada caso
-        temp_cresc[i] = calcular_tempo(cres_cpy, size, algoritmo);
-        temp_dec[i] = calcular_tempo(dec_cpy, size, algoritmo);
-        temp_rand[i] = calcular_tempo(rand_cpy, size, algoritmo);
-        
-        // Verificando se os vetores foram ordenados corretamente
-        int cresc_sorted = esta_ordenado(cres_cpy, size);
-        int dec_sorted = esta_ordenado(dec_cpy, size);
-        int rand_sorted = esta_ordenado(rand_cpy, size);
-        
-        // Imprimindo resultados
-        printf("| %7d | %14.2f | %15.2f | %13.2f |\n", 
-               size, temp_cresc[i], temp_dec[i], temp_rand[i]);
-        
-        // Verificação de ordenação
-        if (!cresc_sorted || !dec_sorted || !rand_sorted) {
-            printf("ERRO: Falha na ordenação para o tamanho %d\n", size);
-            if (!cresc_sorted) printf("  - Array crescente não ordenado corretamente\n");
-            if (!dec_sorted) printf("  - Array decrescente não ordenado corretamente\n");
-            if (!rand_sorted) printf("  - Array aleatório não ordenado corretamente\n");
+                soma_cresc[i] += temp_cresc[i];
+                soma_dec[i] += temp_dec[i];
+                soma_rand[i] +=  temp_rand[i];
+                
+                // Liberando memória
+                free(crescente);
+                free(decrescente);
+                free(random);
+                free(cres_cpy);
+                free(dec_cpy);
+                free(rand_cpy);
+            }
+            
         }
-        
-        // Liberando memória
-        free(crescente);
-        free(decrescente);
-        free(random);
-        free(cres_cpy);
-        free(dec_cpy);
-        free(rand_cpy);
-    }
-    
-    printf("-------------------------------------------------------------\n\n");
-}
 
-int main() {
+        // Imprimindo a média dos tempos
+        printf("media dos tempos de execução (ms):\n");
+        printf("-------------------------------------------------------------\n");
+        for (int i = 0; i < num_sizes; i++) {
+            printf("| %7d | %14.2f | %15.2f | %13.2f |\n", 
+                   sizes[i], soma_cresc[i] / REPETICOES, soma_dec[i] /REPETICOES, soma_rand[i] / REPETICOES);
+        }
+
+        printf("-------------------------------------------------------------\n\n");
+
+        }
+
+        
+    int main() {
     // Tamanhos dos conjuntos de teste
     int sizes[] = {20000, 40000, 60000};
     int num_sizes = sizeof(sizes) / sizeof(sizes[0]);
     
     srand(time(NULL));
 
-    //testar_algoritmo("Insertion Sort", sizes, num_sizes, insertion_sort);
+    testar_algoritmo("Insertion Sort", sizes, num_sizes, insertion_sort);
     
-    testar_algoritmo("Cocktail Shaker Sort", sizes, num_sizes, cocktail_shaker_sort);
+    //star_algoritmo("Cocktail Shaker Sort", sizes, num_sizes, cocktail_shaker_sort);
     
     return 0;
 }
